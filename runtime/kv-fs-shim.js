a94b65bd557e1bde6da77ea9c447663ab1d7c13a
Markus Ahlstrand <markus@ahlstrand.es>
2020-09-22 15:53:33 +0200

    Added getWithMetadata to fs shim

--- runtime/kv-fs-shim.js
+++ runtime/kv-fs-shim.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const mime = require('mime');
 
 module.exports = class KvFsShim {
   constructor({ namespace }) {
@@ -10,6 +11,25 @@ module.exports = class KvFsShim {
     try {
       const value = fs.readFileSync(path.join(this.namespace, key)).toString();
       return value;
+    } catch (err) {
+      return null;
+    }
+  }
+
+  async getWithMetadata(key) {
+    try {
+      const value = fs.readFileSync(path.join(this.namespace, key)).toString();
+      const metadata = {
+        headers: {
+          'Content-Type': mime.getType(path.extname(key)),
+        },
+      };
+
+      return {
+        status: 200,
+        value,
+        metadata,
+      };
     } catch (err) {
       return null;
     }
